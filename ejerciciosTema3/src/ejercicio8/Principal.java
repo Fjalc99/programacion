9c11607a20bdcacea11f398f6f519443744fd6eb
Francisco José Alcantarilla Calado <alcantarilla.cafra24@triana.salesianos.edu>
2024-01-24 21:24:49 +0100

    Actu

--- ejerciciosTema3/src/ejercicio8/Principal.java
+++ ejerciciosTema3/src/ejercicio8/Principal.java
@@ -1,10 +1,46 @@
 package ejercicio8;
 
+import utilidades.Leer;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		double dinRet, dinIng, saldo;
+		int opcion;
+		
+		CuentaCorriente c = new CuentaCorriente (saldo);
+		
+		
+		do {
+			System.out.println("Pulse 1 para ingresar dinero");
+			
+			opcion=Leer.datoInt();
+			
+			switch(opcion) {
+				
+			case 1:
+				
+				System.out.println("Introduce el dinero que desea ingresar");
+				dinIng=Leer.datoDouble();
+				
+				
+			
+				
+			
+			}
+			
+			
+			
+		}while(opcion != 0);
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
