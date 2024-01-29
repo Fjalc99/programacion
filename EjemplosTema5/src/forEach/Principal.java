4ab8449b2e19ddf25180c062ca124f0aff493bdb
Fran <alcantarilla.cafra24@triana.salesianos.edu>
2024-01-29 12:32:03 +0100

    EjemploTema5

---
+++ EjemplosTema5/src/forEach/Principal.java
@@ -0,0 +1,23 @@
+package forEach;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int [] numeros = {1,2,3,4,5,6,7,8,9,10};
+		int suma=0;
+		
+		for (int i : numeros) {
+			suma = suma + i;
+			
+			System.out.println((i)); 
+			//Valor se refiere al valor guardado en el array y no el indice como en los bucles normales. 
+			//numeros es el nombre del array
+		}
+		
+		System.out.println("La suma es: "+suma);
+	}
+
+}
