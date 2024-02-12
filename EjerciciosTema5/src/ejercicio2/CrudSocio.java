9fb9acd352b88ad79ee35b62ec10ec3a4973d133
Fran <alcantarilla.cafra24@triana.salesianos.edu>
2024-02-12 12:36:31 +0100

    a

--- EjerciciosTema5/src/ejercicio2/CrudSocio.java
+++ EjerciciosTema5/src/ejercicio2/CrudSocio.java
@@ -5,6 +5,25 @@ import java.util.List;
 public class CrudSocio {
 
 	private List <Socio> lista = new ArrayList <Socio> ();
+
+	public CrudSocio(List<Socio> lista) {
+		super();
+		this.lista = lista;
+	}
+
+	public List<Socio> getLista() {
+		return lista;
+	}
+
+	public void setLista(List<Socio> lista) {
+		this.lista = lista;
+	}
+
+	@Override
+	public String toString() {
+		return "CrudSocio [lista=" + lista + "]";
+	}
+	
 	
 	
 	
