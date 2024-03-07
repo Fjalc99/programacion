1789945eaa8f0efd50317d1448e69b562f0c0ebf
Fran <alcantarilla.cafra24@triana.salesianos.edu>
2024-03-07 10:59:50 +0100

    a

---
+++ EjemploFechas/src/ejemplo/Principal.java
@@ -0,0 +1,38 @@
+package ejemplo;
+
+import java.time.LocalDate;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//Localdate
+		
+		LocalDate hoy = LocalDate.now();
+		
+		System.out.println(hoy);
+		
+		
+		LocalDate fecha1 = LocalDate.of(1999, 07, 20);
+		
+		System.out.println(fecha1);
+		
+		String fechaParse = "1998-04-08";
+		
+		LocalDate fecha2 = LocalDate.parse(fechaParse);
+		
+		System.out.println(fecha2);
+
+		System.out.println(fecha1.getDayOfMonth());
+		
+		
+		//-----------------------------------------------------------------
+		
+		//Localtime
+		
+	
+	}
+
+}
