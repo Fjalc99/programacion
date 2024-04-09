1503e142ab6b3cb896178d771e0b564120d1ab6f
Fran <alcantarilla.cafra24@triana.salesianos.edu>
2024-04-09 13:27:31 +0200

    a

---
+++ ProyectoFinal/src/clasesPojo/Producto.java
@@ -0,0 +1,9 @@
+package clasesPojo;
+
+public class Producto {
+
+	private String nombre, imagen, talla, descripcionProducto;
+	private int idProducto, cantProductos;
+	private double precio;
+	
+}
---
+++ ProyectoFinal/src/clasesPojo/Socio.java
@@ -0,0 +1,11 @@
+package clasesPojo;
+
+import java.time.LocalDate;
+
+public class Socio {
+	
+	private String nombre, primerApellido, SegundoApellido, sexo, dni, numTelefono, usuario, contraseña, correoElectronico, cuentaBancaria;
+	private int idSocio, edad;
+	private LocalDate fechaNacimiento;
+	private boolean privilegiosSocio;
+}
