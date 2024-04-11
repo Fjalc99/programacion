06ace0cd83485b52b08da714c744f6bc45d8a52e
Fran <alcantarilla.cafra24@triana.salesianos.edu>
2024-04-11 14:09:21 +0200

    a

---
+++ calculadoraRamas/src/ejemploCalculadora/Principal.java
@@ -0,0 +1,47 @@
+package ejemploCalculadora;
+
+import utilidades.Leer;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double num1, num2;
+		int opcion;
+		
+		System.out.println("Introduzca el primer numero");
+		num1=Leer.datoDouble();
+		
+		System.out.println("Introduzca el segundo numero");
+		num2=Leer.datoDouble();
+		
+		
+		do {
+			System.out.println("1 para sumar");
+			System.out.println("2 para restar");
+			System.out.println("3 para multiplicar");
+			System.out.println("4 para dividir");
+			opcion=Leer.datoInt();
+			
+			
+			
+			switch(opcion) {
+				
+				
+			
+			
+			
+			
+			}
+			
+			
+			
+			
+		} while (opcion!=0);
+		
+		
+		
+	}
+
+}
