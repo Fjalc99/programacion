9092073ccdb9aad9318fb7dca2c2f5b9ad55a2b3
Fran <alcantarilla.cafra24@triana.salesianos.edu>
2024-04-22 12:14:14 +0200

    a

--- ExamenUD4FranciscoJoseAlcantarillaCalado/src/ejercicio/AeroPuerto.java
+++ ExamenUD4FranciscoJoseAlcantarillaCalado/src/ejercicio/AeroPuerto.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class AeroPuerto {
 
 	private String nombre;
-	private Avion [] lista;
-	
+	private Avion[] lista;
+
 	public AeroPuerto(String nombre, Avion[] lista) {
 		super();
 		this.nombre = nombre;
@@ -33,70 +33,67 @@ public class AeroPuerto {
 	public String toString() {
 		return "AeroPuerto [nombre=" + nombre + ", lista=" + Arrays.toString(lista) + "]";
 	}
-	
-	
-	public Avion buscarPorId (int id) {
-		
-		int i=0;
+
+	public Avion buscarPorId(int id) {
+
+		int i = 0;
 		boolean encontrado = false;
-		
-		while(i < lista.length && !encontrado) {
-			if (lista[i].getId()==id) {
-				encontrado=true;
-			}else {
+
+		while (i < lista.length && !encontrado) {
+			if (lista[i].getId() == id) {
+				encontrado = true;
+			} else {
 				i++;
 			}
 		}
-		
+
 		if (encontrado) {
 			return lista[i];
-		}else {
+		} else {
 			return null;
 		}
 	}
-	
-	
-	public double calcularAterrizajeUnAvion (Avion v, double porcentaje, double extraPorMisil) {
-		
+
+	public double calcularAterrizajeUnAvion(Avion v, double porcentaje, double extraPorMisil) {
+
 		return v.calcularPrecioAterrizaje(porcentaje, extraPorMisil);
 	}
-	
-	
-	public void mostrarLista () {
-		
+
+	public void mostrarLista() {
+
 		for (int i = 0; i < lista.length; i++) {
-			if (lista[i] !=null) {
+			if (lista[i] != null) {
 				System.out.println(lista[i]);
+
+				if (lista[i] instanceof NavesEspaciales) {
+					((NavesEspaciales) lista[i]).avisarExtraterrestres();
+				}
 			}
-			
-			if (lista[i] instanceof NavesEspaciales) {
-				((NavesEspaciales)lista[i]).avisarExtraterrestres();
-			}
+
 		}
 	}
-	
-	
+
 	public double calcularTotalRecaudado(double porcentaje, double extraPorMisil) {
-		double total=0;
-		
+		double total = 0;
+
 		for (int i = 0; i < lista.length; i++) {
-			if (lista[i]!=null) {
-				total+=lista[i].calcularPrecioAterrizaje(porcentaje, extraPorMisil);
+			if (lista[i] != null) {
+				total += lista[i].calcularPrecioAterrizaje(porcentaje, extraPorMisil);
 			}
 		}
-		
+
 		return total;
 	}
-	
+
 	public double calcularTotalRecaudadoCazas(double porcentaje, double extraPorMisil) {
-		double total=0;
-		
+		double total = 0;
+
 		for (int i = 0; i < lista.length; i++) {
-			if (lista[i] instanceof CazasMilitares && lista[i] !=null) {
-				total +=((CazasMilitares)lista[i]).calcularPrecioAterrizaje(porcentaje, extraPorMisil);
+			if (lista[i] instanceof CazasMilitares && lista[i] != null) {
+				total += ((CazasMilitares) lista[i]).calcularPrecioAterrizaje(porcentaje, extraPorMisil);
 			}
 		}
-		
+
 		return total;
 	}
 }
